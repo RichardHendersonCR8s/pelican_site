ce292cb278e34bc2ac076f8b965ad49834f03ad2
Richard Henderson <RichardHendersonCr8s@gmail.com>
2022-07-27 21:11:26 -0700

    Color Change

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,12 +1,12 @@
 AUTHOR = 'Richard Henderson'
-SITENAME = "Richard Henderson's Blog"
+SITENAME = "From the mind of Ricard Henderson..."
 SITEURL = 'http://RichardHenderson.dev'
 SITETITLE = "Richard Henderson"
 SITESUBTITLE = "Data Analyst"
-SITEDESCRIPTION = "Richard Henderson's thoughts and content."
+SITEDESCRIPTION = "From the mind of Richard Henderson..."
 
 #BROWSER_COLOR =
-
+PYGMENTS_STYLE = "monokai"
 
 PATH = 'content'
 
@@ -21,15 +21,28 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+
+USE_FOLDER_AS_CATEGORY = False
+MAIN_MENU = True
+HOME_HIDE_TAGS = True
+
+MENUITEMS = (
+    ("Archives", "/archives.html"),
+    ("Categories", "/categories.html"),
+    ("Tags", "/tags.html"),
+)
+
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+
+#LINKS = (('Pelican', 'https://getpelican.com/'),
+#('Python.org', 'https://www.python.org/'),
+#('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+#('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('github', 'https://github.com/RichardHendersonCR8s'),
+          ('twitter', 'https://twitter.com/richsUsername_'),
+          ('linkedin','https://www.linkedin.com/in/richard-henderson-716010207/'))
 
 DEFAULT_PAGINATION = 10
 
@@ -44,3 +57,7 @@ SITEDESCRIPTION = "Blog by Richard Henderson. Theme provided by David Bryant Cop
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
+
+COPYRIGHT_YEAR = datetime.now().year
+
+USE_LESS = True
